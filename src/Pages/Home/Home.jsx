6cf1865dc1f4616bdd6e0e476d86650a5c1bbe80
jxstduncan <@Bergmans.duncan@gmail.com>
2025-06-20 14:23:51 +0200

    css fait pour home et button occupé a faire mtn

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -5,9 +5,11 @@ function Home(props){
             <div className="df">
                 {props.data.map((item , index)=>(
                     <div key={index}>
+                       {item?.rating >=4 && <img src={item?.images[0]} alt="" className="bgc"/> }
+                       {item?.rating >=4 && <h3 className="center">{item?.rating} ⭐</h3> }
+
                         <Link to='/ProductDetail' className="textdeco">
-                       {item?.rating >=4 && <img src={item?.images[0]} alt=""/> }
-                       {item?.rating >=4 && <h3 className="center">{item?.rating} </h3> }
+                       {item?.rating >=4 &&  <button>Details</button>}
                         </Link>
                     </div>
                     
